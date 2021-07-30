1fa6832d5240dfeeb83db3c63252a9529a35581e
Guillermo Avendano-Franco <gufranco@mail.wvu.edu>
2021-07-30 05:33:31 -0400

    parallel

---
+++ files/parallel/example_01.f90
@@ -0,0 +1,14 @@
+program main
+
+   implicit none
+   character(len=10) :: i[-2:2, 2, 1:*]
+
+   if (this_image() .eq. num_images()) then
+      write (*, *) "this_image()", this_image()
+      write (*, *) "this_image( i )", this_image(i)
+      write (*, *) "lcobound( i )", lcobound(i)
+      write (*, *) "ucobound( i )", ucobound(i)
+      write (*, *) "image_index(ucobound(i))", image_index(i, ucobound(i))
+   end if
+
+end program main
